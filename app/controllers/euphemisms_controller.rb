b2eb6d7cee1608b104fe3a42f00a75abb3ca18ee
Matthew F. Short <short.matthew.f@gmail.com>
2015-03-23 12:28:48 -0400

    hey

--- app/controllers/euphemisms_controller.rb
+++ app/controllers/euphemisms_controller.rb
@@ -6,4 +6,16 @@ class EuphemismsController < ApplicationController
     render json: @euphemisms
   end
 
+  def create
+    @euphemism = Euphemism.find_or_create_by(euphemism_params)
+
+    render json: @euphemism
+  end
+
+  private
+
+  def euphemism_params
+    params.require(:euphemism).permit(:verb_id, :adjective_id, :noun_id)
+  end
+
 end
