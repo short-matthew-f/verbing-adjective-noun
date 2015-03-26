e19ffc745bee7698e7158c304e2d197b674b8f57
Matthew F. Short <short.matthew.f@gmail.com>
2015-03-25 20:42:36 -0400

    consistency is key

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   root to: "euphemisms#new"
 
   get '/words/fetch', to: "words#fetch", as: "fetch_words"
-
-  get '/euphemisms/new'
   get '/euphemisms/fetch'
   get '/euphemisms/popular'
   get '/euphemisms/:id/like', to: "euphemisms#like", as: "like_euphemism"
   get '/euphemisms/:id/dislike', to: "euphemisms#dislike", as: "displike_euphemism"
 
-  resources :euphemisms, only: [:index, :create]
+  resources :euphemisms, only: [:new, :index, :create]
 end
